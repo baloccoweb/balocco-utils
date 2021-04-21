1b090fbd5dcd3554b60b088c13aee3bf1537950b
Stefano Gava <gava97@gmail.com>
2021-04-21 22:42:15 +0200

    UNIEURO add some headers

--- utils/headers.js
+++ utils/headers.js
@@ -33,8 +33,15 @@ const getAmdDefaultHeaders = (customHeaders = []) => {
 }
 
 const getUnieuroDefaultHeaders = (customHeaders = []) => {
+    const locale = getLocale();
+
     return [
         `user-agent: ${getRandomUserAgent()}`,
+        'accept: application/json',
+        'accept-encoding: gzip, deflate, br',
+        `accept-language: ${locale},${locale.substr(0, 2)};q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6`,
+        "cache-control: no-cache",
+        "pragma: no-cache",
     ].concat(customHeaders);
 }
 
