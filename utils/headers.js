9a04953f6d2a3b7d0c29478d7ab90f63d6a684ee
Stefano Gava <gava97@gmail.com>
2021-04-29 19:10:49 +0200

    AMD HEADERS: remove languafe and encoding

--- utils/headers.js
+++ utils/headers.js
@@ -7,8 +7,6 @@ const getAmdDefaultHeaders = (customHeaders = []) => {
     return [
         `user-agent: ${randomUserAgent({ browser: 'firefox' })}`,
         "accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8",
-        `accept-language: ${locale},${locale.substr(0, 2)};q=0.8,en-US;q=0.5,en;q=0.3`,
-        "accept-encoding: gzip, deflate, br",
         "upgrade-insecure-requests: 1",
         "cache-control: no-cache",
         "pragma: no-cache",
