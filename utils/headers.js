8c8952640df1edd89c0d899af3c3f1a85da844c1
Stefano Gava <gava97@gmail.com>
2021-04-29 20:23:56 +0200

    UNIEURO HEADERS: remove accept encoding

--- utils/headers.js
+++ utils/headers.js
@@ -7,7 +7,6 @@ const getUnieuroDefaultHeaders = (customHeaders = []) => {
     return [
         `user-agent: ${randomUserAgent()}`,
         'accept: application/json',
-        'accept-encoding: gzip, deflate, br',
         `accept-language: ${locale},${locale.substr(0, 2)};q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6`,
         "cache-control: no-cache",
         "pragma: no-cache",
