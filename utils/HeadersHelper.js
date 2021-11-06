9529b67751483a9e07ee3ded4081d62cf76656e5
Stefano Gava <gava97@gmail.com>
2021-11-06 18:19:34 +0100

    Improve HeadersHelper

--- utils/HeadersHelper.js
+++ utils/HeadersHelper.js
@@ -5,7 +5,7 @@ const CHROME_HEADERS = {
     'cache-control': 'no-cache',
     'sec-ch-ua': '"Google Chrome";v="${CHROME_VERSION}", " Not;A Brand";v="99", "Chromium";v="${CHROME_VERSION}"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform': '"${PLATFORM}"',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (${OS}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${CHROME_BUILD} Safari/537.36',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -18,28 +18,44 @@ const CHROME_HEADERS = {
 
 //https://chromereleases.googleblog.com/
 const REAL_CHROME_VERSIONS = [
+    [89, "89.0.4389.90"],
+    [90, "90.0.4430.93"],
+    [91, "91.0.4472.77"],
+    [91, "91.0.4472.114"],
+    [92, "92.0.4515.107"],
     [92, "92.0.4515.159"],
     [93, "93.0.4577.63"],
     [93, "93.0.4577.82"],
     [94, "94.0.4606.41"],
+    [94, "94.0.4606.61"],
     [95, "95.0.4636.4"],
     [95, "95.0.4638.69"]
 ];
 
 const OS = [
     'Windows NT 10.0; Win64; x64',
+    'X11; Linux x86_64',
+    'Macintosh; Intel Mac OS X 10_15_7'
     //TODO
 ];
 
 const randomUA = () => {
-    let ua = CHROME_HEADERS['user-agent'];
     const chomeVersion = REAL_CHROME_VERSIONS[randomIntFromInterval(0, REAL_CHROME_VERSIONS.length - 1)];
     const osVersion = OS[randomIntFromInterval(0, OS.length - 1)];
 
-    ua = CHROME_HEADERS['user-agent'].replaceAll("${CHROME_BUILD}", chomeVersion[1])
+    let ua = CHROME_HEADERS['user-agent'].replaceAll("${CHROME_BUILD}", chomeVersion[1])
         .replaceAll("${OS}", osVersion);
 
-    return { ua, version: chomeVersion[0] };
+    let platform = CHROME_HEADERS['sec-ch-ua-platform'];
+    if (osVersion.includes("Windows")) {
+        platform = platform.replace('${PLATFORM}', 'Windows');
+    } else if (osVersion.includes("Linux")) {
+        platform = platform.replace('${PLATFORM}', 'Linux');
+    } else {
+        platform = platform.replace('${PLATFORM}', 'macOS');
+    }
+
+    return { ua, version: chomeVersion[0], platform };
 };
 
 const getRealChrome = (locale) => {
@@ -49,12 +65,13 @@ const getRealChrome = (locale) => {
         .replace("${LOCALE_FULL}", locale + "-" + locale.toUpperCase())
         .replace("${LOCALE}", locale);
 
-    const { ua, version } = randomUA();
+    const { ua, version, platform } = randomUA();
 
     newHeaders["sec-ch-ua"] = newHeaders["sec-ch-ua"]
         .replaceAll("${CHROME_VERSION}", version);
 
     newHeaders["user-agent"] = ua;
+    newHeaders["sec-ch-ua-platform"] = platform;
 
     return newHeaders;
 }
