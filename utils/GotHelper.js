98c1dda7f1738c3004a9d0a260cf6ab7eacab821
Stefano <stefano@agaweb.it>
2022-04-29 18:57:55 +0200

    Fix SocksProxyAgent import

--- utils/GotHelper.js
+++ utils/GotHelper.js
@@ -1,6 +1,6 @@
 const HeadersHelper = require("./HeadersHelper");
 const HttpsProxyAgent = require("https-proxy-agent");
-const SocksProxyAgent = require("socks-proxy-agent");
+const { SocksProxyAgent } = require("socks-proxy-agent");
 
 const GotHelper = {
     getDefaultOptions: (locale, agent, timeout = 5000) => {
@@ -8,7 +8,7 @@ const GotHelper = {
             headers: HeadersHelper.getRealChrome(locale),
             throwHttpErrors: false,
             http2: !agent || !!agent.http2,
-            timeout: {request: timeout},
+            timeout: { request: timeout },
             ...(agent ? { agent } : {})
         };
     },
