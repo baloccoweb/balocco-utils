78b500d5afdd4db15cc24b1f58d28e1e012dd4c0
Stefano Gava <gava97@gmail.com>
2022-09-17 16:35:27 +0200

    Updates

--- utils/HeadersHelper.js
+++ utils/HeadersHelper.js
@@ -18,6 +18,7 @@ const CHROME_HEADERS = {
 
 //https://chromereleases.googleblog.com/
 const REAL_CHROME_VERSIONS = [
+    [105, "105.0.0.0"],
     [104, "104.0.0.0"],
     [103, "103.0.5060.66"],
     [103, "103.0.0.0"],
@@ -25,8 +26,7 @@ const REAL_CHROME_VERSIONS = [
     [101, "101.0.4951.41"],
     [99, "99.0.4844.84"],
     [99, "99.0.4844.82"],
-    [98, "98.0.4758.109"],
-    [98, "98.0.4758.102"]
+    [98, "98.0.4758.109"]
 ];
 
 const OS = [
@@ -83,15 +83,16 @@ const FIREFOX_TYPES = {
 };
 
 const REAL_FIREFOX_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.6; rv:104.0) Gecko/20100101 Firefox/104.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.5; rv:103.0) Gecko/20100101 Firefox/103.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.4; rv:102.0) Gecko/20100101 Firefox/102.0",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 12.3; rv:99.0) Gecko/20100101 Firefox/99.0",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0",
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0"
 ];
 
 const defaultFirefoxXhrLowerHeaders = {
